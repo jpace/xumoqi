83c425db6936b53562103b2e3220c615ba65cddc
Jeff Pace <jeugenepace@gmail.com>
2014-01-25 19:05:03 -0500

    Removed abstract method.

--- src/org/incava/xumoqi/GameDottedWords.java
+++ src/org/incava/xumoqi/GameDottedWords.java
@@ -4,10 +4,5 @@ public abstract class GameDottedWords extends Game {
 	public GameDottedWords(WordList wordList, int length, int nDots) {
 		super(wordList, length, nDots);
 	}
-
-	@Override
-	public String getAsPattern(String word) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
+
